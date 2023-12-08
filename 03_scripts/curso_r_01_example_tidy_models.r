d6b0e3a0981018e49286c130f5489e5978e3bad1
maykongpedro <54289045+maykongpedro@users.noreply.github.com>
2023-12-07 22:51:04 -0300

    add model adjusts and results

--- 03_scripts/curso_r_01_example_tidy_models.r
+++ 03_scripts/curso_r_01_example_tidy_models.r
@@ -40,8 +40,6 @@ dados_diamantes |>
   # gerar um resumo de correlação das variáveis
   GGally::ggpairs()
 
-
-
 # Precisamos passar pro R:
 # 1. A f que queremos usar
 # 2. Ajustar a f para um conjunto de dados
@@ -81,3 +79,92 @@ especificacao_modelo_random_forest <-
 
 # Passo 2: Ajuste do modelo ---------------------------------
   
+modelo <- especificacao_modelo |>
+  # y (price) em função da variável x
+  parsnip::fit(price ~ x, data = dados_diamantes)
+
+# ver modelo
+modelo
+
+# ver a árvore de regressão
+rpart.plot::prp(modelo$fit)
+
+
+
+# Passo 3: Analisar previsões ---------------------------------
+
+# obter e acessar previsão dos dados
+# previsao <- predict(modelo, new_data = dados_diamantes)
+# previsao$.pred
+
+
+diamantes_com_previsao <- dados_diamantes |>
+  dplyr::mutate(
+    # criar coluna com a previsao dos dados geralmente aqui o argumento new_data
+    # recebe a base de teste, e não a base de treino, para realmente validar a
+    # previsão do modelo
+    price_previsao = predict(modelo, new_data = dados_diamantes)$.pred
+  ) |>
+  print()
+
+
+# plotar pontos observados + curva f
+diamantes_com_previsao |>
+  dplyr::filter(x > 0) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(x = x, y   = price),
+    alpha = 0.3
+  ) +
+  ggplot2::geom_step(
+    mapping = ggplot2::aes(x = x, y = price_previsao),
+    color = "red", 
+    linewidth = 1
+   ) +
+  ggplot2::theme_bw()
+  
+
+# plotar pontos observados x esperados
+diamantes_com_previsao |>
+  dplyr::filter(x > 0) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(x = price_previsao, y = price)  
+  ) +
+  ggplot2::geom_abline(
+    slope = 1,
+    intercept = 0,
+    color = "purple"
+  ) +
+  ggplot2::theme_bw()
+
+
+# Passo 4 - Como quantificar a qualidade de um modelo? ---------------------------------
+
+# Métricas de erro
+# erro quadrático médio: cerca de 1500$ por diamante
+diamantes_com_previsao |> 
+  yardstick::rmse(
+    truth = price, 
+    estimate = price_previsao
+  )
+
+# Resíduo = truth-estimate
+# erro absoluto médio: cerca de 850$ por diamante
+diamantes_com_previsao |> 
+  yardstick::mae(
+    truth = price, 
+    estimate = price_previsao
+  )
+
+# R quadrado: cerca de 86% da variabilidade está sendo explicada com o modelo
+diamantes_com_previsao |> 
+  yardstick::rsq(
+    truth = price, 
+    estimate = price_previsao
+  )
+
+# esse modelo tem um problema com heterasticidade pois os preços grandes já
+# possuam uma distribuiçao diferente da normal, a mairo parte dos dados está nos
+# preços pequenos, o que acaba gerando uma boa estatística de erro mas na
+# prática ainda erramos muito quando os preços são grandes
