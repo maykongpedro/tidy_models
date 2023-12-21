e150f776fa2500cdc89e78e27102cbdddab35a20
maykongpedro <54289045+maykongpedro@users.noreply.github.com>
2023-12-21 20:34:57 -0300

    finish the class adding a test with production data

--- 03_scripts/curso_r_02_example_overfitting.R
+++ 03_scripts/curso_r_02_example_overfitting.R
@@ -4,9 +4,10 @@
 source(
   here::here(
     "03_scripts",
-    "00_install_packcages"
+    "00_install_packcages.R"
   )
 )
+library(patchwork)
 
 
 # Get data ---------------------------------
@@ -114,3 +115,203 @@ diamondiszinho_c_previsoes_long |>
     truth = price,
     estimate = price_pred
   )
+
+
+# plotar gráfico dos pontos observados + curva f
+p1_diamondiszinho_c_previsoes <- diamondiszinho_c_previsoes |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(
+      x = x, 
+      y = price
+    ),
+    size = 3
+  ) +
+  ggplot2::geom_step(
+    mapping = ggplot2::aes(
+      x = x,
+      y = price_pred_mod1,
+      color = "Modelo 1"
+    ),
+    linewidth = 1
+  ) +
+  ggplot2::geom_step(
+    mapping = ggplot2::aes(
+      x = x,
+      y = price_pred_mod2,
+      color = "Modelo 2 - Overffiting"
+    ),
+    linewidth = 1
+  )
+p1_diamondiszinho_c_previsoes
+
+
+# plotar gráfico de observado x esperado
+diamondiszinho_c_previsoes |>
+  tidyr::pivot_longer(
+    cols = starts_with("price_pred"),
+    names_to = "modelo",
+    values_to = "price_pred"
+  ) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(
+      x = price_pred, 
+      y = price,
+      color = modelo
+    ),
+    size = 3
+  ) +
+  ggplot2::geom_abline(
+    slope = 1,
+    intercept = 0,
+    color = "purple"
+  ) +
+  ggplot2::theme_bw()
+
+
+# plotar gráfico de resíduos x esperado por esse modelo não pressupostos, o
+# formato dos resíduos não implica em nada a única garantia teórica que temos é
+# que o erro que conseguirmos em uma base de teste, geralmente vai ser próximo
+# do erro que conseguirmos com uma base em produção
+p2_diamondiszinho_c_previsoes <- diamondiszinho_c_previsoes |>
+  tidyr::pivot_longer(
+    cols = starts_with("price_pred"),
+    names_to = "modelo",
+    values_to = "price_pred"
+  ) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(
+      x = price_pred, 
+      y = price - price_pred,
+      color = modelo
+    ),
+    size = 3
+  ) +
+  ggplot2::geom_abline(
+    slope = 0,
+    intercept = 0,
+    color = "purple"
+  ) +
+  ggplot2::ylim(c(-10000, 10000)) +
+  ggplot2::labs(
+    y = "Resíduos (y - y_chapeu)"
+  )
+  ggplot2::theme_bw()
+
+p2_diamondiszinho_c_previsoes
+
+
+
+
+
+# ======================================================================
+# Extra: Simular uma etapa de produção ---------------------------------
+# ======================================================================
+
+
+set.seed(3)
+# dados novos chegaram...
+diamantes_novos <- dados_diamantes |>
+  dplyr::filter(x > 0) |>
+  dplyr::sample_n(100)
+
+
+# predições 
+diamantes_novos_previsoes <- diamantes_novos |>
+  dplyr::mutate(
+    price_pred_mod1 = stats::predict(ajuste_modelo1, new_data = diamantes_novos)$.pred,
+    price_pred_mod2 = stats::predict(ajuste_modelo2_of, new_data = diamantes_novos)$.pred,
+  )
+
+# pivotar para gerar as métricas de erro
+diamantes_novos_previsoes_long <- diamantes_novos_previsoes |>
+  tidyr::pivot_longer(
+    cols = starts_with("price_pred"),
+    names_to = "modelo",
+    values_to = "price_pred"
+  )
+
+# estimativa de erro por modelo
+diamantes_novos_previsoes_long |>
+  dplyr::group_by(modelo) |>
+  yardstick::rmse(
+    truth = price,
+    estimate = price_pred
+  )
+
+# r quadrado por modelo (de 0 a 1)
+diamantes_novos_previsoes_long |>
+  dplyr::group_by(modelo) |>
+  yardstick::rsq(
+    truth = price,
+    estimate = price_pred
+  )
+
+# plotar gráfico de pontos observados + curva f
+p1_diamantes_novos_previsoes <- diamantes_novos_previsoes |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(
+      x = x, 
+      y = price
+    ),
+    size = 3
+  ) +
+  ggplot2::geom_step(
+    mapping = ggplot2::aes(
+      x = x,
+      y = price_pred_mod1,
+      color = "Modelo 1"
+    ),
+    linewidth = 1
+  ) +
+  ggplot2::geom_step(
+    mapping = ggplot2::aes(
+      x = x,
+      y = price_pred_mod2,
+      color = "Modelo 2 - Overffiting"
+    ),
+    linewidth = 1
+  )
+
+# plotar o gráfico da base original com os dados novos
+p1_diamondiszinho_c_previsoes / p1_diamantes_novos_previsoes
+
+
+# plotar gráfico de resíduos x esperado
+p2_diamantes_novos_previsoes <- diamantes_novos_previsoes |>
+  tidyr::pivot_longer(
+    cols = starts_with("price_pred"),
+    names_to = "modelo",
+    values_to = "price_pred"
+  ) |>
+  ggplot2::ggplot() +
+  ggplot2::geom_point(
+    mapping = ggplot2::aes(
+      x = price_pred, 
+      y = price - price_pred,
+      color = modelo
+    ),
+    size = 3
+  ) +
+  ggplot2::geom_abline(
+    slope = 0,
+    intercept = 0,
+    color = "purple"
+  ) +
+  ggplot2::ylim(c(-10000, 10000)) +
+  ggplot2::labs(
+    y = "Resíduos (y - y_chapeu)"
+  )
+  ggplot2::theme_bw()
+
+
+# plotar o gráfico da base original com os dados novos
+p2_diamondiszinho_c_previsoes / p2_diamantes_novos_previsoes
+
+
+# podemos observar que o modelo com overffiting dessa vez não se comportou bem.
+# Uma vez que ele basicamente se adaptou totalmente aos dados de treino, nào
+# conseguiu se encontrar na base de teste (produção)
